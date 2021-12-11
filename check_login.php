03c01a0dc90e475c112fab226bd8fb94e4419bfd
andrey-ab <al-spb777@mail.ru>
2021-12-11 18:41:00 +0300

    AB

--- check_login.php
+++ check_login.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    include("C:\\AppParams\\params.php");
+    include(getenv("MYAPP_CONFIG"));
 ?>
 <html>
     <head>
@@ -17,8 +17,8 @@
             ";
 
             $conn = mysqli_connect($DB_URL,$DB_USER,$DB_PWD,$DB_NAME);
-            //Нудная, но необходимая процедура передачи параметров 
-            //в sql выражение, что гарантирует защиту от инжекции sql
+            // Нудная, но необходимая процедура передачи параметров 
+            // в sql выражение, что гарантирует защиту от инжекции sql
             $statement = mysqli_prepare($conn, $sql);
             mysqli_stmt_bind_param($statement,"ss",$user,$hash);
             mysqli_stmt_execute($statement);
