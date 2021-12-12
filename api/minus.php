2c755da2981aa434656c7b5b1f2ec3f065cebdc3
andrey-ab <al-spb777@mail.ru>
2021-12-12 23:25:33 +0300

    AB

--- api/minus.php
+++ api/minus.php
@@ -8,7 +8,7 @@ $z = $x - $y;
 $user = $_SESSION["user"];
 
 $conn = mysqli_connect("$DB_URL","$DB_USER","$DB_PWD","$DB_NAME");
-$sql = "INSERT INTO log(Number1, Number2, Result, UserID) VALUES($x,$y,$z,'$user')";
+$sql = "INSERT INTO log(Number1,Number2,Result,UserID) VALUES($x,$y,$z,'$user')";
 mysqli_query($conn, $sql);
 mysqli_close($conn);
 
