1faebb0e9a44c5055e0f95b62b2657766faaa3ed
andrey-ab <al-spb777@mail.ru>
2021-12-12 22:16:49 +0300

    AB

--- api/plus.php
+++ api/plus.php
@@ -1,24 +1,15 @@
 <?php
-
-//Это комментарий
+session_start();
+include (getenv('MYAPP_CONFIG'));
 
 $x = $_REQUEST["x"];
 $y = $_REQUEST["y"];
 $z = $x + $y;
+$user = $_SESSION["user"];
 
-// Здесь нарушены принципы безопасности:
-// 1. Принцип наименьших привилегий
-// 2. Слабый пароль
-// 3. Секрет в коде
-//$conn = mysqli_connect("localhost","root","","calc");
-// 4. Код, уязвимый для Sql-injection
-
-//Первые три проблемы исправлены ниже:
-include(getenv("MYAPP_CONFIG"));
-$conn = mysqli_connect($DB_URL,$DB_USER,$DB_PWD,$DB_NAME);
-
-$sql = "INSERT INTO log(Number1,Number2,Result,UserID) VALUES($x,$y,$z,'anonym')";
-mysqli_query($conn,$sql);
-//echo(mysqli_error($conn));
+$conn = mysqli_connect("$DB_URL","$DB_USER","$DB_PWD","$DB_NAME");
+$sql = "INSERT INTO log(Number1, Number2, Result, UserID) VALUES($x,$y,$z,'$user')";
+mysqli_query($conn, $sql);
 mysqli_close($conn);
-echo($z);
+
+echo ($z);
